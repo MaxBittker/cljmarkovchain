cb7090079439bd3a0e037b6157f06f9c87e3d3b9
maxbittker <maxbittker@gmail.com>
2016-05-31 13:52:35 -0400

    sean and leo code review changes

--- src/cljmarkovchain/core.clj
+++ src/cljmarkovchain/core.clj
@@ -1,32 +1,32 @@
 (ns cljmarkovchain.core
-  (:gen-class))
+  (:gen-class)
+  (require [clojure.string :as str]))
 
-(require '[clojure.string :as str])
-
-(defn updateWord
+(defn update-word
   "inserts a suffix to a word map entry"
   [sMap suffix]
-  (assoc sMap suffix (inc (get sMap suffix 0))))
+  (assoc sMap suffix (inc (sMap suffix 0))))
 
 (defn insert
   "inserts word to word map"
-  [set [word suffix]]
-  (assoc set word (updateWord (get set word (hash-map)) suffix)))
+  [store [word suffix]]
+  (assoc store word (update-word (store word {}) suffix)))
 
-(defn randKey
-  [set]
-  (nth (keys set) (rand-int (count (keys set))) "none"))
+(defn rand-key
+  [store]
+  (nth (keys store) (rand-int (count (keys store)))))
 
 (defn find-first
+  "return first value that matches a predicate"
   [f coll]
   (first (filter f coll)))
 
-(defn weightedKey
+(defn weighted-key
   "Given a map of keys and weights, return a key, selecting based on weights."
-  [set]
-  (let [weights   (reductions + (vals set))
+  [store]
+  (let [weights   (reductions + (vals store))
         total   (last weights)
-        choices (map vector (keys set) weights)]
+        choices (map vector (keys store) weights)]
       (first
         (find-first
             #(> (second %) (rand-int total))
@@ -37,7 +37,7 @@
   (if (zero? n)
       acc
       (recur chain
-             (randKey (get chain position (get chain (randKey chain))))
+             (weighted-key (chain position (chain (rand-key chain))))
              (dec n)
              (str acc " " position))))
 
@@ -48,5 +48,6 @@
         ; (str/split (slurp "./resources/input.txt") #" ")
         (re-seq #"\w+" (slurp "./resources/input.txt"))
         pairs (partition 2 1 tokens)
-        markov (reduce insert (hash-map) pairs)]
-    (println (walk markov (randKey markov) 1000 "start: "))))
+        chain (reduce insert {} pairs)]
+    (println chain)
+    (println (take 10 (repeatedly #(walk chain (rand-key chain) 15 "\n"))))))
